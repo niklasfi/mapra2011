339591912876903710e0371105178104bd3a6340
Niklas Fischer <niklasfi@gmail.com>
2011-04-12 19:15:55 +0200

    Materialien für Aufgabe 2 hinzugefügt

---
+++ ma2/unit2.h
@@ -0,0 +1,25 @@
+/********************************************************************
+*  Name       : unit2.h                                             *
+*  Verwendung : Umgebung zu Praktikumsaufgabe (MAPRA),              *
+*               Sortieralgorithmen                                  *
+*  Autor      : K. Brix, IGPM RWTH Aachen                           *
+*  Datum      : Mar '08                                             *
+*  Sprache    : C++                                                 *
+********************************************************************/
+
+#ifndef unit2
+#define unit2
+
+#include <iostream>
+
+// Konstanten
+
+extern const int AnzahlBeispiele;
+
+// Funktionen
+
+void start(const unsigned int beispiel, unsigned int &laenge, unsigned int *&feld);
+bool ergebnis(unsigned int *feld);
+void tausche(unsigned int *feld, const long int i, const long int j);
+
+#endif
