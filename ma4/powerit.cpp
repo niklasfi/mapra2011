c61eebd557769e3a9790ccfafdb7f29a0a4f2ef6
Niklas Fischer <niklasfi@gmail.com>
2011-05-16 15:05:40 +0200

    power iteration

---
+++ ma4/powerit.cpp
@@ -0,0 +1,43 @@
+#include "unit4.h"
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	if(argc == 1){
+		std::cout << "usage: powerit.exe bspNummer [eps]\n";
+		exit(1);
+	}
+	
+	int bsp = ui2::readarg<int>((std::string)argv[1],"konnte Beispielnummer liegt nicht im richtigen Zahlenbereich.", ui2::check_range<int>(1,AnzahlBeispiele));
+
+	Matrix A; Vektor x[2]; double eps;
+	
+	Start (bsp, A, x[0], eps );
+
+	x[1].ReDim(x[0].Laenge());
+	
+	Vektor xTild[2];
+	
+	xTild[0].ReDim(x[0].Laenge());
+	xTild[1].ReDim(x[0].Laenge());
+	
+	xTild[0](0) = 1.0/0.0; //stellt sicher, dass man nicht sofort aus der while-schleife rausfliegt
+
+	int i = 1, k = 0;
+	
+	while(true){
+		xTild[i % 2] = A * x[(i-1) % 2];
+		
+		x[i % 2] = xTild[i % 2] / xTild[i % 2](k);
+		
+		if ((x[0]-x[1]).NormMax() < eps && fabs(xTild[0](k) - xTild[1](k)) < eps) break;
+		
+		int max;
+		if(2 * fabs(xTild[i % 2](k)) <= xTild[i % 2].NormMax(max))
+			k = max;
+		
+		i++;
+	}
+	
+	Ergebnis ( x[i%2], xTild[i%2](k), i );
+}
