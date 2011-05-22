0d2793b36ead02fc93fd74639fa51261bfc43466
Jee Siang Soh <sohjeesiang@gmail.com>
2011-05-23 00:23:28 +0200

    maxnorm von Matrix

---
+++ ma4/maxnorm.cpp
@@ -0,0 +1,22 @@
+
+// ----- Maximum-Norm -----
+
+double Matrix::NormMax() const
+{
+    // ***** Hier fehlt was *****
+	
+	double zsum;
+	double zsummax = 0;
+	
+	for(int i = 0; i < Zeil; i++){
+		zsum = 0;
+	
+		for (int j = 0; j < Spalt; j++)
+			zsum += fabs(Mat(i,j));			// Summieren der Zeileneintraeger einzeln zum Betrag
+		
+		if (zsummax < zsum) zsummax = zsum;
+	}
+	
+	return zsummax;
+}
+
