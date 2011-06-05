330ff6f0254367f3ca026cb643686a8a0aab3e33
Jee Siang Soh <sohjeesiang@gmail.com>
2011-06-05 18:40:32 +0200

    mit neuer 32-bit Datei

---
+++ ma5/RuKu.cpp
@@ -0,0 +1,19 @@
+/*****************************************************
+*  Name       : RuKu.cpp                             *
+*  Verwendung : Runge-Kutta-Verfahren		         *
+*  Autor      : Jee Siang, Soh; Niklas Fischer       *
+*  Datum      : Mai/ Juni '2011                      *
+*****************************************************/
+
+
+#include "tvektor.h"
+#include "tmatrix.h"
+#include "RuKu.h"
+
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+
+
---
+++ ma5/RuKu.h
@@ -0,0 +1,22 @@
+#ifndef _RUKU_H
+#define _RUKU_H
+
+#include <iostream>
+
+class RuKu
+{
+
+private:
+
+
+
+
+public:
+
+
+
+
+
+}
+
+#endif
